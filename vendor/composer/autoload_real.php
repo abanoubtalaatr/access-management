ab357815ccc36cbb3dcdf5e1d63a4d101463ba8f
abanoub-talaat <105149048+abanoub-talaat@users.noreply.github.com>
2024-09-08 09:39:52 +0300

    WIP "

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitcfeb879a47c400ebb2dc6991203a92ed
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitcfeb879a47c400ebb2dc6991203a92ed', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitcfeb879a47c400ebb2dc6991203a92ed', 'loadClassLoader'));
@@ -30,6 +32,18 @@ class ComposerAutoloaderInitcfeb879a47c400ebb2dc6991203a92ed
         call_user_func(\Composer\Autoload\ComposerStaticInitcfeb879a47c400ebb2dc6991203a92ed::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitcfeb879a47c400ebb2dc6991203a92ed::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
